b34f85f4f3071174b7fbf523a0289673e9782bd7
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-05-09 21:21:29 -0500

    updates on server

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-var fs = require('fs')
+var {readFile} = require('fs/promises')
 var path = require('path')
 var express = require('express')
-var uuid = require('uuid')
+var {v4:uuidv4} = require('uuid')
+const { networkInterfaces } = require('os')
+const getNotes=()=>{ 
+return readFile('db/db.json','utf-8').then(notes =>[].concat(JSON.parse(notes)))
+}
 
 var app = express()
 
 var PORT= process.env.PORT || 3001
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
-app.get('/test', (req,res) =>{
-    res.send('hello')
-})
+// app.get('/test', (req,res) =>{
+//     res.send('hello')
+// })
 
 //Route skurrrr 
 app.get('/notes', function(req,res){
-    res.sendFile(path.join(_dirname,'/public/notes.html'))
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
 })
 
-app.get('api/notes',function(req,res){
-fs.readFile('../db.db.json,utf-8',function(err,data){
-    if(err) throw err
-    res.send(data)
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/index.html'))
 })
+
+
+app.get('/api/notes',function(req,res){ 
+    getNotes() .then(notes=>res.json(notes)).catch(err=>res.json(err))
 })
-app.post('/api/notes',function(res,res){
-    var title=req.body.title
-    var text=req.body.text
-    
-    fs.readFile('../db.db.json,utf-8',function(err,data){
-        if(err) throw err
+app.post('/api/notes',function(req,res){
+    getNotes().then(oldNotes =>{
+        var {title,text}= req.body
+        var newNote={title,text,id:uuidv4()}
+        var notearry= [...oldNotes,newNote]
+        console.log(notearry)
+    })
+
 }) 
+app.listen(PORT,()=>{
+console.log('http://localhost:'+ PORT)
+
+
+})
