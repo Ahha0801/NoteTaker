54a245ab716df6d943e19e2f7f05e9d3f59f4275
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-05-14 18:07:27 -0500

    updated second page saving

--- server.js
+++ server.js
@@ -1,48 +1,72 @@
-var {readFile} = require('fs/promises')
-var path = require('path')
-var express = require('express')
-var {v4:uuidv4} = require('uuid')
+// Import necessary modules
+const { readFile } = require('fs/promises')
+const path = require('path')
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 const { networkInterfaces } = require('os')
-const getNotes=()=>{ 
-return readFile('db/db.json','utf-8').then(notes =>[].concat(JSON.parse(notes)))
+const fs = require('fs');
+
+// Define a function to read notes from file
+const getNotes = () => { 
+  return readFile('db/db.json', 'utf-8')
+    .then(notes => [].concat(JSON.parse(notes)))
 }
 
-var app = express()
+// Create a new Express app
+const app = express()
 
-var PORT= process.env.PORT || 3001
+// Define the port to listen on
+const PORT = process.env.PORT || 3001
 
+// Use middleware to parse JSON and urlencoded request bodies
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+
+// Serve static files from the public directory
 app.use(express.static('public'))
 
-app.get('/test', (req,res) =>{
-    res.send('hello')
+// Define a test route
+app.get('/test', (req, res) => {
+  res.send('hello')
 })
 
-//Route skurrrr 
-app.get('/notes', function(req,res){
-    res.sendFile(path.join(__dirname,'./public/notes.html'))
+// Define a route to serve the notes page
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname,'./public/index.html'))
+// Define a route to serve the home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-
-app.get('/api/notes',function(req,res){ 
-    getNotes() .then(notes=>res.json(notes)).catch(err=>res.json(err))
+// Define a route to get all notes
+app.get('/api/notes', (req, res) => { 
+  getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.json(err))
 })
-app.post('/api/notes',function(req,res){
-    getNotes().then(oldNotes =>{
-        var {title,text}= req.body
-        var newNote={title,text,id:uuidv4()}
-        var notearry= [...oldNotes,newNote]
-        console.log(notearry)
-    })
 
-}) 
-app.listen(PORT,()=>{
-console.log('http://localhost:'+ PORT)
+// Define a route to add a new note
+app.post('/api/notes', (req, res) => {
+  getNotes().then(oldNotes => {
+    // Extract the title and text from the request body
+    const { title, text } = req.body
+    // Create a new note object with a unique ID
+    const newNote = { title, text, id: uuidv4() }
+    // Add the new note to the array of old notes
+    const noteArray = [...oldNotes, newNote]
+    console.log(noteArray)
+    const noteArrayString= JSON.stringify(noteArray)
+    fs.writeFile('./db/db.json',noteArrayString, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    res.json(newNote)
+  })
+})
 
-
+// Start listening for requests
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
 })
